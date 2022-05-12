06226e870f7e7eae4e47fa6124717c2f69c7041f
Priyanka <priyankahansi95@gmail.com>
2022-05-12 13:18:39 +0530

    added:mobile number

--- src/com/bridgelabz/RegEx.java
+++ src/com/bridgelabz/RegEx.java
@@ -4,10 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegEx {
-    static Pattern pattern = Pattern.compile("abc.[a-z]{3,}@bl.co.[a-z]{2}$");
+    static Pattern pattern = Pattern.compile("[0-9]{2} [0-9]{10}");
 
     public static void main(String[] args) {
-        String testString = "abc.zzz@bl.co.in";
+        String testString = "91 9999999999";
         Matcher matcher = pattern.matcher(testString);
 
         if (matcher.matches()) {
