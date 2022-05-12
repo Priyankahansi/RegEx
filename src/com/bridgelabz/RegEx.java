62cddd70231e704c8aa20ce381244cbce5c1c756
Priyanka <priyankahansi95@gmail.com>
2022-05-12 19:05:06 +0530

    added:password rules

--- src/com/bridgelabz/RegEx.java
+++ src/com/bridgelabz/RegEx.java
@@ -4,10 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegEx {
-    static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){1,}[0-9a-zA-Z]{7,}$");
+    static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){3,}@[0-9a-zA-Z]$");
 
     public static void main(String[] args) {
-        String testString = "Praju111";
+        String testString = "09pp@11";
         Matcher matcher = pattern.matcher(testString);
 
         if (matcher.matches()) {
