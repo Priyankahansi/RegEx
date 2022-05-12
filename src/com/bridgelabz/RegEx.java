67097d36b14ce217acbc00070fdbc7a6809b26a4
Priyanka <priyankahansi95@gmail.com>
2022-05-12 12:58:43 +0530

    added:LastName

--- src/com/bridgelabz/RegEx.java
+++ src/com/bridgelabz/RegEx.java
@@ -7,7 +7,7 @@ public class RegEx {
     static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
 
     public static void main(String[] args) {
-        String testString = "ABB";
+        String testString = "PRAJUHANASI";
         Matcher matcher = pattern.matcher(testString);
 
         if (matcher.matches()) {
